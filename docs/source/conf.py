ca2439715ac4adc28dbcd50e9f38d9bafb9aa475
adeslatt <adeslat@scitechcon.org>
2026-02-03 12:51:04 -0500

    debugging auto docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,3 @@
-# Configuration file for Sphinx documentation
-
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
@@ -13,8 +11,21 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',  # Add this
     'myst_parser',
 ]
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+# Mock imports for packages that might not be available during build
+autodoc_mock_imports = []  # Add any problematic imports here if needed
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -22,6 +33,7 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# Napoleon settings for Google/NumPy docstrings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
+napoleon_use_param = True
+napoleon_use_rtype = True
